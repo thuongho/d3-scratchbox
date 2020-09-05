c7e53762acb639ed2335a8485a7f87369a5e1984
Thuong Ho <thuong.t.ho@gmail.com>
2020-09-05 15:25:34 -0700

    Make Scatterplot render when props are updated
    convert App to class
    set initial width and height in state
    add onClick method that makes w and h 20% smaller
    update Scatterplot to Purecomponent
    move xScale and yScale to state
    add getDerivedStateFromProps lifecycle to render d3

--- scatter-plot/src/App.js
+++ scatter-plot/src/App.js
@@ -3,16 +3,46 @@ import * as d3 from 'd3';
 import Scatterplot from './Scatterplot';
 import './App.css';
 
-function App() {
-  const data = d3.range(100).map((_) => [Math.random(), Math.random()]);
+const data = d3.range(100).map((_) => [Math.random(), Math.random()]);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      width: 300,
+      height: 200
+    };
+  }
 
-  return (
-    <div className='App'>
-      <svg width='800' height='800'>
-        <Scatterplot x={50} y={50} height={300} width={300} data={data} />
-      </svg>
-    </div>
-  );
+  onClick = () => {
+    this.setState({
+      width: this.state.width * 0.8,
+      height: this.state.height * 0.8
+    });
+  };
+
+  render() {
+    const { width, height } = this.state;
+    return (
+      <div className='App'>
+        <svg width='800' height='800' onClick={this.onClick}>
+          <Scatterplot
+            x={50}
+            y={50}
+            height={height}
+            width={width}
+            data={data}
+          />
+          <Scatterplot
+            x={50}
+            y={330}
+            height={height}
+            width={width}
+            data={data}
+          />
+        </svg>
+      </div>
+    );
+  }
 }
 
 export default App;
--- scatter-plot/src/Scatterplot.jsx
+++ scatter-plot/src/Scatterplot.jsx
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import * as d3 from 'd3';
 
 import Axis from './Axis';
 
-class Scatterplot extends Component {
-  xScale = d3.scaleLinear().domain([0, 1]).range([0, this.props.width]);
+// PureComponent so that React is smarter about calling getDerivedStateFromProps
+class Scatterplot extends PureComponent {
+  state = {
+    xScale: d3.scaleLinear().domain([0, 1]).range([0, this.props.width]),
+    yScale: d3.scaleLinear().domain([0, 1]).range([this.props.height, 0])
+  };
 
-  yScale = d3.scaleLinear().domain([0, 1]).range([this.props.height, 0]);
+  // this lifecycle method gets called whenever React renders Scatterplot
+  static getDerivedStateFromProps(props, state) {
+    let { xScale, yScale } = state;
+
+    xScale.range([0, props.width]);
+    yScale.range([0, props.height]);
+
+    return { ...state, xScale, yScale };
+  }
 
   render() {
     const { x, y, height, data } = this.props;
+    const { xScale, yScale } = this.state;
 
     return (
       <g transform={`translate(${x}, ${y})`}>
         {data.map(([x, y]) => {
           return (
-            <circle
-              key={`${x}-${y}`}
-              cx={this.xScale(x)}
-              cy={this.yScale(y)}
-              r='5'
-            />
+            <circle key={`${x}-${y}`} cx={xScale(x)} cy={yScale(y)} r='5' />
           );
         })}
-        <Axis x={0} y={0} type='Left' scale={this.yScale} label='Y' />
-        <Axis x={0} y={height} type='Bottom' scale={this.xScale} label='X' />
+        <Axis x={0} y={0} type='Left' scale={yScale} label='Y' />
+        <Axis x={0} y={height} type='Bottom' scale={xScale} label='X' />
       </g>
     );
   }
