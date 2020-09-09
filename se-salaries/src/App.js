934d3d8359b8337c9919485e8c582f62f4830623
Thuong Ho <thuong.t.ho@gmail.com>
2020-09-09 12:09:38 -0700

    Simplify app state

--- se-salaries/src/App.js
+++ se-salaries/src/App.js
@@ -4,25 +4,56 @@ import _ from 'lodash';
 
 import Preloader from './components/Preloader';
 import { loadAllData } from './DataHandling';
+import CountyMap from './components/CountyMap';
 
 function App() {
   // define what's in component state in advance to set expectations
-  const [techSalaries, setTechSalaries] = useState([]);
-  const [medianIncomes, setMedianIncomes] = useState([]);
-  const [countyNames, setCountyNames] = useState([]);
+  const [datasets, setDatasets] = useState({
+    techSalaries: [],
+    medianIncomes: [],
+    countyNames: [],
+    usTopoJSON: null,
+    USStateNames
+  });
+  const {
+    techSalaries,
+    medianIncomes,
+    countyNames,
+    usTopoJSON,
+    USStateNames
+  } = datasets;
 
   const loadData = async () => {
     try {
-      const data = await loadAllData();
-
-      const { techSalaries, medianIncomes, countyNames } = data;
-
-      setTechSalaries(techSalaries);
-      setMedianIncomes(medianIncomes);
-      setCountyNames(countyNames);
+      const datasets = await loadAllData();
+      setDatasets(datasets);
     } catch (e) {
       console.error('No data available.', e);
     }
+  };
+
+  /**
+   * Method that takes a county entry and a map of tech salaries
+   * it then returns the delta between median household income and a single tech salary
+   * @function countyValue
+   * @param {Object} county
+   * @param {Object} techSalariesMap
+   * @returns {Object}
+   */
+  const countyValue = (county, techSalariesMap) => {
+    // medianIncomes to get median household salary
+    const medianHousehold = medianIncomes[county.id],
+      // techsalariesmap to get salaries for specific census area
+      salaries = techSalariesMap[county.name];
+
+    if (!medianHousehold || !salaries) return null;
+
+    const median = d3.median(salaries, (d) => d.base_salary);
+
+    return {
+      countyID: county.id,
+      value: median - medianHousehold.medianIncome
+    };
   };
 
   useEffect(() => {
@@ -30,15 +61,36 @@ function App() {
     loadData();
   }, []);
 
+  // Render
+
   if (techSalaries.length < 1) {
     return <Preloader />;
-  } else {
-    return (
-      <div className='App container'>
-        <h1>Loaded {techSalaries.length} salaries</h1>
-      </div>
-    );
   }
+
+  const filteredSalaries = techSalaries,
+    filteredSalariesMap = _.groupBy(filteredSalaries, 'countyID'),
+    countyValues = countyNames
+      .map((county) => countyValue(county, filteredSalariesMap))
+      .filter((d) => !_.isNull(d));
+
+  let zoom = null;
+
+  return (
+    <div className='App container'>
+      <svg width='1100' height='500'>
+        <CountyMap
+          usTopoJson={usTopoJSON}
+          USStateNames={USStateNames}
+          values={countyValues}
+          x={0}
+          y={0}
+          width={500}
+          height={500}
+          zoom={zoom}
+        />
+      </svg>
+    </div>
+  );
 }
 
 export default App;
