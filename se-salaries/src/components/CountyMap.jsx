916e58185de0663c69989c6bc6fa02a5c8c63a65
Thuong Ho <thuong.t.ho@gmail.com>
2020-09-09 17:06:36 -0700

    Use mesh and feature to add borders and flats to counties

--- se-salaries/src/components/CountyMap.jsx
+++ se-salaries/src/components/CountyMap.jsx
@@ -64,7 +64,38 @@ const CountyMap = ({
 
   // TopoJSON is a geographical data format based on JSON
   if (!usTopoJSON) return null;
-  return;
+
+  const us = usTopoJSON,
+    // borders
+    USStatesMesh = topojson.mesh(us, us.objects.states, (a, b) => a !== b),
+    // flat areas
+    counties = topojson.feature(us, us.objects.counties).features;
+
+  // optimize by building a map beforehand
+  const countyValueMap = _.fromPairs(values.map((d) => [d.countyID, d.value]));
+
+  return (
+    <g>
+      {counties.map((feature) => (
+        <County
+          geoPath={geoPath}
+          feature={feature}
+          zoom={zoom}
+          key={feature.id}
+          quantize={quantize}
+          value={countyValueMap[feature.id]}
+        />
+      ))}
+      <path
+        d={geoPath(USStatesMesh)}
+        style={{
+          fill: 'none',
+          stroke: '#fff',
+          strokeLinejoin: 'round'
+        }}
+      />
+    </g>
+  );
 };
 
 export default CountyMap;
